b50697d1aefcfb3d39db146702b82a34d974ce68
Mikle <miroshnikov.m@gmail.com>
2017-09-26 19:07:22 +0300

    v.1003

--- app/controllers/httpCtrl.js
+++ app/controllers/httpCtrl.js
@@ -8,6 +8,9 @@ myApp.controller('httpCtrl', function($scope, $http, $timeout, sharePO) {
     $scope.message = "httpCtrl message";
 
 
+
+
+
     // OK   get PO from server and create table in PO.html
     $scope.getAllProjectData = function() {
         $scope.getProjectData = [];
@@ -25,17 +28,7 @@ myApp.controller('httpCtrl', function($scope, $http, $timeout, sharePO) {
 
 
 
-    // OK  delete item from array
-    $scope.deleteItem = function(array, item) {
 
-        var index = array.indexOf(item);
-
-        if (index > -1) {
-            $scope.ind = index;
-            array.splice(index, 1);
-            $scope.newArray = array;
-        } else { alert("error from slice"); }
-    };
 
 
     // OK  create new PO - PO.html
@@ -55,7 +48,7 @@ myApp.controller('httpCtrl', function($scope, $http, $timeout, sharePO) {
 
                 $http.post("/api/PO", data).then(function(response) { // api_project_route.js: apiRoutes.put('/PO', requireAuth, AuthenticationController.roleAuthorization(['admin']), function(req, res) {
 
-                    if (response.data.error.code) { // all error check
+                    if (response.data.error) { // all error check
 
                         $scope.errmsg = response.data.error.errmsg; // show message
 
@@ -73,14 +66,53 @@ myApp.controller('httpCtrl', function($scope, $http, $timeout, sharePO) {
 
             } // end if
             else {
-                alert('wrong input');
+
+                $scope.errmsg = 'введите корректный номер проекта'; // show message
+
+                $timeout(function() { // hide error in 3,5 sec
+                    $scope.errmsg = false;
+                }, 3500);
+
             }
 
         } // end function
 
 
+
+
+
+    $scope.hideDeleteButton = true;
+    $scope.confirmDeletePO = false;
+
+    $scope.setConfirmDeletePO = function() {
+        $scope.confirmDeletePO = true;
+        $scope.hideDeleteButton = true;
+    }
+
+
+
+    // OK  delete item from array
+    $scope.deleteItem = function(array, item) {
+
+        var index = array.indexOf(item);
+
+        if (index > -1) {
+            $scope.ind = index;
+            array.splice(index, 1);
+            $scope.newArray = array;
+
+        } else { alert("error from slice"); }
+
+
+    }; // end deleteItem
+
+
+
+
     // OK delete PO
     $scope.deletePO = function(PO) {
+
+
 
         $http.delete("/api/" + PO).then(function(response) {
             console.log(response);
@@ -90,6 +122,8 @@ myApp.controller('httpCtrl', function($scope, $http, $timeout, sharePO) {
         }).then(function() {
 
         });
+
+
     };
 
 
@@ -101,6 +135,8 @@ myApp.controller('httpCtrl', function($scope, $http, $timeout, sharePO) {
 
         });
     };
+
+
 
 
 
